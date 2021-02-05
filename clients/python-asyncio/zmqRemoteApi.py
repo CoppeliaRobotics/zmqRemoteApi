d256582a43cfd86d3c905cd2ef38c51c3a475b90
Federico Ferri <federico.ferri.it@gmail.com>
2021-02-05 16:21:06 +0100

    add async client

--- clients/python-asyncio/zmqRemoteApi.py
+++ clients/python-asyncio/zmqRemoteApi.py
@@ -1,6 +1,8 @@
 import json
 import cbor
+import asyncio
 import zmq
+import zmq.asyncio
 
 def deepmapitem(fn, d):
     if isinstance(d, (list, tuple)):
@@ -12,28 +14,28 @@ def deepmapitem(fn, d):
 
 class RemoteAPIClient:
     def __init__(self, host='localhost', port=23000, *, verbose=False):
-        self.context = zmq.Context()
+        self.context = zmq.asyncio.Context()
         self.socket = self.context.socket(zmq.REQ)
         self.verbose = verbose
         self.socket.connect('tcp://{}:{}'.format(host, port))
     def __del__(self):
         self.socket.close()
         self.context.term()
-    def call(self, func, args, verbose=None):
+    async def call(self, func, args, verbose=None):
         if verbose is None: verbose = self.verbose
         req = {'func': func, 'args': args}
         if verbose: print(req)
-        self.socket.send(cbor.dumps(req))
-        resp = cbor.loads(self.socket.recv())
+        await self.socket.send(cbor.dumps(req))
+        resp = cbor.loads(await self.socket.recv())
         resp = deepmapitem(lambda k, v: (k.decode('utf8'), v), resp)
         if verbose: print(resp)
         if not resp.get('success', False): raise Exception(resp.get('error'))
         ret = resp['ret']
         if len(ret) == 1: return ret[0]
         if len(ret) > 1: return tuple(ret)
-    def getobject(self, name, _info=None):
+    async def getobject(self, name, _info=None):
         ret = type(name, (), {})
-        if not _info: _info = self.call('zmqRemoteApi.info', [name])
+        if not _info: _info = await self.call('zmqRemoteApi.info', [name])
         for k, v in _info.items():
             if not isinstance(v, dict): raise ValueError('found nondict')
             if len(v) == 1 and 'func' in v:
@@ -44,10 +46,12 @@ class RemoteAPIClient:
                 setattr(ret, k, self.getobject(f'{name}.{k}', _info=v))
         return ret
 
-if __name__ in ('__main__', '__console__'):
-    client = RemoteAPIClient()
-    sim = client.getobject('sim')
+async def main():
+    client = RemoteAPIClient(verbose=False)
+    sim = await client.getobject('sim')
+    print(await sim.getObjectHandle('Floor'))
+    print(await sim.unpackTable(await sim.packTable({'a': 1, 'b': 2})))
+    print(await sim.getObjectHandle('foo'))
+
 if __name__ in ('__main__',):
-    print(sim.getObjectHandle('Floor'))
-    print(sim.unpackTable(sim.packTable({'a': 1, 'b': 2})))
-    print(sim.getObjectHandle('foo'))
+    asyncio.run(main())
