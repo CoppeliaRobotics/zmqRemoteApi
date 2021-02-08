eeebe45f77d3a215da8df1331ab5c0894e839d82
Federico Ferri <federico.ferri.it@gmail.com>
2021-02-08 16:24:33 +0100

    fix linter issues

--- clients/python-asyncio/zmqRemoteApi.py
+++ clients/python-asyncio/zmqRemoteApi.py
@@ -1,43 +1,63 @@
-import json
-import cbor
+"""CoppeliaSim's Remote API client."""
+
 import asyncio
+
+import cbor
+
 import zmq
 import zmq.asyncio
 
-def deepmapitem(fn, d):
-    if isinstance(d, (list, tuple)):
-        return type(d)(deepmapitem(fn, x) for x in d)
-    elif isinstance(d, dict):
-        return {k: deepmapitem(fn, v) for k, v in map(lambda t: fn(*t), d.items())}
-    else:
-        return d
 
 class RemoteAPIClient:
+    """Client to connect to CoppeliaSim's ZMQ Remote API."""
+
     def __init__(self, host='localhost', port=23000, *, verbose=False):
+        """Create client and connect to the ZMQ Remote API server."""
         self.context = zmq.asyncio.Context()
         self.socket = self.context.socket(zmq.REQ)
         self.verbose = verbose
-        self.socket.connect('tcp://{}:{}'.format(host, port))
+        self.socket.connect(f'tcp://{host}:{port}')
+
     def __del__(self):
+        """Disconnect and destroy client."""
         self.socket.close()
         self.context.term()
+
     async def call(self, func, args, verbose=None):
-        if verbose is None: verbose = self.verbose
+        """Call function with specified arguments."""
+        def deepmapitem(fn, d):
+            if isinstance(d, (list, tuple)):
+                return type(d)(deepmapitem(fn, x) for x in d)
+            elif isinstance(d, dict):
+                return {k: deepmapitem(fn, v) for k, v in map(lambda t: fn(*t), d.items())}
+            else:
+                return d
+        if verbose is None:
+            verbose = self.verbose
         req = {'func': func, 'args': args}
-        if verbose: print(req)
+        if verbose:
+            print(req)
         await self.socket.send(cbor.dumps(req))
         resp = cbor.loads(await self.socket.recv())
         resp = deepmapitem(lambda k, v: (k.decode('utf8'), v), resp)
-        if verbose: print(resp)
-        if not resp.get('success', False): raise Exception(resp.get('error'))
+        if verbose:
+            print(resp)
+        if not resp.get('success', False):
+            raise Exception(resp.get('error'))
         ret = resp['ret']
-        if len(ret) == 1: return ret[0]
-        if len(ret) > 1: return tuple(ret)
+        if len(ret) == 1:
+            return ret[0]
+        if len(ret) > 1:
+            return tuple(ret)
+
     async def getobject(self, name, _info=None):
+        """Retrieve remote object from server."""
         ret = type(name, (), {})
-        if not _info: _info = await self.call('zmqRemoteApi.info', [name])
+        if not _info:
+            _info = await self.call('zmqRemoteApi.info', [name])
         for k, v in _info.items():
-            if not isinstance(v, dict): raise ValueError('found nondict')
+            if not isinstance(v, dict):
+                raise ValueError('found nondict')
             if len(v) == 1 and 'func' in v:
                 setattr(ret, k, lambda *a, func=f'{name}.{k}': self.call(func, a))
             elif len(v) == 1 and 'const' in v:
@@ -46,7 +66,9 @@ class RemoteAPIClient:
                 setattr(ret, k, self.getobject(f'{name}.{k}', _info=v))
         return ret
 
+
 async def main():
+    """Test basic usage."""
     client = RemoteAPIClient(verbose=False)
     sim = await client.getobject('sim')
     print(await sim.getObjectHandle('Floor'))
