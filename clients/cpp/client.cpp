2aa2ac62781cb7e95fc472b3dec29a975b28cf4c
Federico Ferri <federico.ferri.it@gmail.com>
2021-09-15 19:05:54 +0200

    class RemoteAPIClient

--- clients/cpp/client.cpp
+++ clients/cpp/client.cpp
@@ -1,26 +1,79 @@
 #include <iostream>
-#include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
+#include <boost/format.hpp>
 #include <zmq.hpp>
+#include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
+class RemoteAPIClient
+{
+public:
+    RemoteAPIClient(const std::string host = "localhost", const int port = 23000, bool verbose_ = false)
+        : sock(ctx, zmq::socket_type::req),
+          verbose(verbose_)
+    {
+        auto addr = (boost::format("tcp://%s:%d") % host % port).str();
+        sock.connect(addr);
+    }
+
+    json call(const std::string &func, const json &args = json::array())
+    {
+        json req;
+        req["func"] = "sim.getObjectHandle";
+        req["args"] = args;
+        send(req);
+
+        json resp = recv();
+        bool ok = resp["success"].get<bool>();
+        if(!ok)
+        {
+            if(resp.contains("error"))
+                throw std::runtime_error(resp["error"].get<std::string>());
+            else
+                throw std::runtime_error("unknown error");
+        }
+        const auto &ret = resp["ret"];
+        return ret;
+    }
+
+protected:
+    void send(const json &j)
+    {
+        if(verbose)
+            std::cout << "Sending: " << j.dump(4) << std::endl;
+
+        std::vector<uint8_t> data = json::to_cbor(j);
+
+        zmq::message_t msg(data.data(), data.size());
+        sock.send(msg, zmq::send_flags::dontwait);
+    }
+
+    json recv()
+    {
+        zmq::message_t msg;
+        sock.recv(msg);
+
+        auto data = reinterpret_cast<const uint8_t*>(msg.data());
+        json j = json::from_cbor(data, data + msg.size(), false);
+
+        if(verbose)
+            std::cout << "Received: " << j.dump(4) << std::endl;
+
+        return j;
+    }
+
+private:
+    bool verbose{false};
+    zmq::context_t ctx;
+    zmq::socket_t sock;
+};
+
 int main()
 {
-    zmq::context_t ctx;
-    zmq::socket_t sock(ctx, zmq::socket_type::req);
-    sock.connect("tcp://localhost:23000");
-
-    json jreq = R"({"func": "sim.getObjectHandle", "args": ["Floor"]})"_json;
-    std::cout << "Sending: " << jreq.dump(4) << std::endl;
-    std::vector<uint8_t> breq = json::to_cbor(jreq);
-    zmq::message_t req(breq.begin(), breq.end());
-    sock.send(req, zmq::send_flags::dontwait);
-
-    zmq::message_t resp;
-    sock.recv(resp);
-    auto data = reinterpret_cast<const uint8_t*>(resp.data());
-    json jresp = json::from_cbor(data, data + resp.size(), false);
-    std::cout << "Received: " << jresp.dump(4) << std::endl;
-
+    RemoteAPIClient client;
+    auto ret = client.call("sim.getObjectHandle", R"(["Floor"])"_json);
+    std::cout << "Ret: " << ret << std::endl;
     return 0;
 }
