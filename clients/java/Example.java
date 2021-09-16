473decfc2889e6019f9b8fd4c75670770e7e27a1
Federico Ferri <federico.ferri.it@gmail.com>
2021-09-16 13:25:58 +0200

    java client

---
+++ clients/java/Example.java
@@ -0,0 +1,12 @@
+import org.json.JSONArray;
+
+public class Example
+{
+    public static void main(String[] _args)
+    {
+        RemoteAPIClient client = new RemoteAPIClient();
+        JSONArray ret = client.call("sim.getObjectHandle", "Floor");
+        for(int i = 0; i < ret.length(); i++)
+            System.out.println("ret[" + i + "]: " + ret.get(i));
+    }
+}
