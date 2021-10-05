86183c3752fe3aaa3c8a547a796e3c3ce92f192d
Federico Ferri <federico.ferri.it@gmail.com>
2021-10-05 10:43:09 +0200

    add python opencv example

---
+++ clients/python/opencv.py
@@ -0,0 +1,58 @@
+# Make sure to have the add-on "ZMQ remote API" running in
+# CoppeliaSim and have following scene loaded:
+#
+# scenes/messaging/synchronousImageTransmissionViaRemoteApi.ttt
+#
+# Do not launch simulation, but run this script
+#
+# All CoppeliaSim commands will run in blocking mode (block
+# until a reply from CoppeliaSim is received). For a non-
+# blocking example, see simpleTest-nonBlocking.py
+
+import time
+
+import numpy as np
+import cv2
+
+from zmqRemoteApi import RemoteAPIClient
+
+
+print('Program started')
+
+client = RemoteAPIClient()
+sim = client.getObject('sim')
+
+visionSensorHandle = sim.getObjectHandle('/VisionSensor')
+passiveVisionSensorHandle = sim.getObjectHandle('/PassiveVisionSensor')
+
+# When simulation is not running, ZMQ message handling could be a bit
+# slow, since the idle loop runs at 8 Hz by default. So let's make
+# sure that the idle loop runs at full speed for this program:
+defaultIdleFps = sim.getInt32Param(sim.intparam_idle_fps)
+sim.setInt32Param(sim.intparam_idle_fps, 0)
+
+# Run a simulation in stepping mode:
+client.setStepping(True)
+sim.startSimulation()
+
+while (t := sim.getSimulationTime()) < 3:
+    img, resX, resY = sim.getVisionSensorCharImage(visionSensorHandle)
+    img = np.frombuffer(img, dtype=np.uint8).reshape(resY, resX, 3)
+
+    # In CoppeliaSim images are left to right (x-axis), and bottom to top (y-axis)
+    # (consistent with the axes of vision sensors, pointing Z outwards, Y up)
+    # and color format is RGB triplets, whereas OpenCV uses BGR:
+    img = cv2.flip(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), 0)
+
+    cv2.imshow('', img)
+    cv2.waitKey(0)
+    client.step()  # triggers next simulation step
+
+sim.stopSimulation()
+
+# Restore the original idle loop frequency:
+sim.setInt32Param(sim.intparam_idle_fps, defaultIdleFps)
+
+cv2.destroyAllWindows()
+
+print('Program ended')
