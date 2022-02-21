413c2a6eeb6c1b07fa8101ff60bd149187eb4577
Federico Ferri <federico.ferri.it@gmail.com>
2022-02-21 13:37:43 +0100

    more examples

---
+++ clients/matlab/README.md
@@ -0,0 +1,11 @@
+# MATLAB ZMQ Remote API
+
+This library contains ZMQ Remote API client for MATLAB.
+
+## Requirements
+
+ - **JeroMQ**: build [JeroMQ](https://github.com/zeromq/jeromq) and add the absolute path of the `jeromq*.jar` file to MATLAB's [`javaclasspath.txt`](https://mathworks.com/help/matlab/matlab_external/static-path-of-java-class-path.html).
+
+## Examples
+
+See [`example.m`](example.m) for a usage example.
--- clients/matlab/example.m
+++ clients/matlab/example.m
@@ -1,4 +1,15 @@
 client = RemoteAPIClient();
 sim = client.getObject('sim');
-[handle] = sim.getObject('/Floor');
-fprintf('Handle of /Floor is %d\n', handle);
+
+% get some object handles:
+visionSensorHandle = sim.getObject('/VisionSensor');
+passiveVisionSensorHandle = sim.getObject('/PassiveVisionSensor');
+
+% read vision sensor image:
+[img, resX, resY] = sim.getVisionSensorCharImage(visionSensorHandle);
+
+% display image in MATLAB:
+imshow(flip(permute(reshape(img, 3, resY, resX), [3 2 1]), 1));
+
+% write image to another sensor:
+sim.setVisionSensorCharImage(passiveVisionSensorHandle, img);
