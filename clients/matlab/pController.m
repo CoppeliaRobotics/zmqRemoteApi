bb985260e3625afebbaee9f3c0ddd59686dbed22
Federico Ferri <federico.ferri.it@gmail.com>
2022-02-22 14:54:11 +0100

    more examples

---
+++ clients/matlab/pController.m
@@ -0,0 +1,54 @@
+% Make sure to have CoppeliaSim running, with followig scene loaded:
+%
+% scenes/messaging/pControllerViaRemoteApi.ttt
+%
+% Do not launch simulation, but run this script
+
+fprintf('Program started\n');
+
+maxForce = 100;
+PID_P = 0.1;
+dynStepSize = 0.005;
+velUpperLimit = deg2rad(360);
+
+client = RemoteAPIClient();
+sim = client.getObject('sim');
+
+jointHandle = sim.getObject('/Cuboid[0]/joint');
+jointAngle = sim.getJointPosition(jointHandle);
+sim.setJointTargetVelocity(jointHandle, deg2rad(360));
+
+% enable the stepping mode on the client:
+client.setStepping(true);
+
+sim.startSimulation();
+
+angles = [
+    45
+    90
+    -89 % no -90, to avoid passing below
+    0
+];
+
+for i=1:numel(angles)
+    targetAngle = deg2rad(angles(i));
+    while abs(jointAngle - targetAngle) > deg2rad(0.1)
+        % Calculate the velocity needed to reach the position
+        % in one dynamic time step:
+        errorValue = targetAngle - jointAngle;
+        sinAngle = sin(errorValue);
+        cosAngle = cos(errorValue);
+        errorValue = atan2(sinAngle, cosAngle);
+        ctrl = errorValue * PID_P;
+        velocity = max(-velUpperLimit, min(velUpperLimit, ctrl / dynStepSize));
+        % .
+        sim.setJointTargetVelocity(jointHandle, velocity);
+        sim.setJointMaxForce(jointHandle, maxForce);
+        client.step();
+        jointAngle = sim.getJointPosition(jointHandle);
+    end
+end
+
+sim.stopSimulation();
+
+fprintf('Program ended\n');
---
+++ clients/matlab/synchronousImageTransmission.m
@@ -0,0 +1,32 @@
+% Make sure to have the add-on "ZMQ remote API" running in
+% CoppeliaSim and have following scene loaded:
+%
+% scenes/messaging/synchronousImageTransmissionViaRemoteApi.ttt
+%
+% Do not launch simulation, but run this script
+%
+
+fprintf('Program started\n');
+
+client = RemoteAPIClient();
+sim = client.getObject('sim');
+
+visionSensorHandle = sim.getObject('/VisionSensor');
+passiveVisionSensorHandle = sim.getObject('/PassiveVisionSensor');
+
+client.setStepping(true);
+sim.startSimulation();
+
+startTime = sim.getSimulationTime();
+t = startTime;
+while t - startTime < 5
+    [img, resX, resY] = sim.getVisionSensorCharImage(visionSensorHandle);
+    % display image in MATLAB:
+    %imshow(flip(permute(reshape(img, 3, resY, resX), [3 2 1]), 1));
+    sim.setVisionSensorCharImage(passiveVisionSensorHandle, img);
+    client.step();
+    t = sim.getSimulationTime();
+end
+sim.stopSimulation();
+
+fprintf('Program ended\n');
