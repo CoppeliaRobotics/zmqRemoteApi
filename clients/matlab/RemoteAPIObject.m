a5ee4bd82dc3f7f970c69dac42f1cfecbef8b757
Federico Ferri <federico.ferri.it@gmail.com>
2022-02-24 18:27:56 +0100

    fetch constants upfront in one shot

--- clients/matlab/RemoteAPIObject.m
+++ clients/matlab/RemoteAPIObject.m
@@ -2,33 +2,44 @@ classdef RemoteAPIObject
     properties (Access = protected)
         x__client
         x__objectName
+        x__info
     end
 
     methods
         function self = RemoteAPIObject(client, objectName)
             self.x__client = client;
             self.x__objectName = objectName;
+            info = client.call('zmqRemoteApi.info', {objectName});
+            self.x__info = info{1};
         end
 
         function varargout = subsref(self, S)
-            fn = self.x__objectName;
-            args = {};
-            func_call = 0;
-            for i=1:numel(S)
-                if strcmp(S(i).type, '.')
-                    fn = strcat(fn, S(i).type, S(i).subs);
-                elseif strcmp(S(i).type, '()') && i == numel(S)
-                    args = S(i).subs;
-                    func_call = 1;
-                else
-                    error('unexpected sub type %s at %d', S(i).type, i);
+            for i=1:numel(S)-1
+                if ~strcmp(S(i).type, '.')
+                    error('invalid field access');
                 end
             end
-            if ~func_call
-                args = {fn};
-                fn = 'zmqRemoteApi.info';
+            switch S(end).type
+                case '.'
+                    info = builtin('subsref', self.x__info, S);
+                    if ~isfield(info, 'const')
+                        error('not a constant');
+                    end
+                    varargout = {info.const};
+                case '()'
+                    info = builtin('subsref', self.x__info, S(1:end-1));
+                    if ~isfield(info, 'func')
+                        error('not a function');
+                    end
+                    fn = self.x__objectName;
+                    for i=1:numel(S)-1
+                        fn = strcat(fn, S(i).type, S(i).subs);
+                    end
+                    args = S(end).subs;
+                    varargout = self.x__client.call(fn, args);
+                otherwise
+                    error('invalid field access');
             end
-            varargout = self.x__client.call(fn, args);
         end
     end
 end
