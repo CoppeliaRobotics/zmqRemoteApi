129d7ad99f47fe3e328a205975bcd61340672f09
Federico Ferri <federico.ferri.it@gmail.com>
2022-03-15 15:45:00 +0100

    restore cmdline arg

--- tools/calltip_test.py
+++ tools/calltip_test.py
@@ -1,9 +1,13 @@
 if __name__ == '__main__':
+    import sys
     from calltip import FuncDef
     from pprint import PrettyPrinter
     pp = PrettyPrinter(indent=4)
     try:
-        func_defs = FuncDef.get_all()
+        if len(sys.argv) > 1:
+            func_defs = FuncDef.get(sys.argv[1:])
+        else:
+            func_defs = FuncDef.get_all()
         pp.pprint(func_defs)
     except Exception as e:
         print(f'error: {e}')
