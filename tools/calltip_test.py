068755b35ed0522417c954855cffef8df70d45c0
Federico Ferri <federico.ferri.it@gmail.com>
2022-04-05 14:15:29 +0200

    fix broken test

--- tools/calltip_test.py
+++ tools/calltip_test.py
@@ -3,11 +3,6 @@ if __name__ == '__main__':
     from calltip import FuncDef
     from pprint import PrettyPrinter
     pp = PrettyPrinter(indent=4)
-    try:
-        if len(sys.argv) > 1:
-            func_defs = FuncDef.get(sys.argv[1:])
-        else:
-            func_defs = FuncDef.get_all()
-        pp.pprint(func_defs)
-    except Exception as e:
-        print(f'error: {e}')
+    c = 'int objectHandle=sim.getObject(string path,map options={})'
+    func_defs = FuncDef.from_calltip(c)
+    pp.pprint(func_defs)
