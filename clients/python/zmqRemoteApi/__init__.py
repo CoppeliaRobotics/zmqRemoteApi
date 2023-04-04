96cf97a6c8e27ab5e2ab98d70588222c6d489d76
Federico Ferri <federico.ferri.it@gmail.com>
2023-04-04 09:34:58 +0200

    add deprecation message

--- clients/python/zmqRemoteApi/__init__.py
+++ clients/python/zmqRemoteApi/__init__.py
@@ -1,3 +1,4 @@
 import sys
 sys.path.append('..')
 from src.coppeliasim_zmqremoteapi_client import *
+print('warning: import zmqRemoteApi is deprecated. Please use import coppeliasim_zmqremoteapi_client instead.')
