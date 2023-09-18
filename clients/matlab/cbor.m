e456caa5a488bb4ab06c3f3ab1fd99c4000bf97c
Federico Ferri <federico.ferri.it@gmail.com>
2023-09-18 12:44:59 +0200

    support for undef.len map/array and a few checks

--- clients/matlab/cbor.m
+++ clients/matlab/cbor.m
@@ -38,13 +38,17 @@ classdef cbor
         FALSE = 20
         TRUE = 21
         NULL = 22
+        UNDEFINED = 23
+        SINGLE_BYTE_EXTENSION = 24
         FLOAT16 = 25
         FLOAT32 = 26
         FLOAT64 = 27
+        BREAK = 31
         UINT8 = 24
         UINT16 = 25
         UINT32 = 26
         UINT64 = 27
+        UNDEFINED_LENGTH = 31
     end
 
     methods(Static, Access = private)
@@ -73,10 +77,11 @@ classdef cbor
             info = bitand(d(1), cbor.MASK_INFO);
             d1 = d(2:end);
             d1 = d1(:)';
-            info1 = double(info);
             as = @(x,t) typecast(flip(x), t);
             if major ~= cbor.SIMPLE_VALUE
-                if info == cbor.UINT8
+                if info < 24
+                    info1 = double(info);
+                elseif info == cbor.UINT8
                     info1 = double(d1(1));
                     d1 = d1(2:end);
                 elseif info == cbor.UINT16
@@ -88,22 +93,32 @@ classdef cbor
                 elseif info == cbor.UINT64
                     info1 = double(as(d1(1:8), 'uint64'));
                     d1 = d1(9:end);
+                elseif info == cbor.UNDEFINED_LENGTH
+                    info1 = NaN;
+                elseif info > 27
+                    error('malformed data (info > 27)');
                 end
             end
             if major == cbor.UNSIGNED_INT
+                if isnan(info1) error('malformed data (int + undefined length)') end
                 o = info1;
             elseif major == cbor.NEGATIVE_INT
+                if isnan(info1) error('malformed data (negative int + undefined length)') end
                 o = -1 - info1;
             elseif major == cbor.BYTE_STRING
+                if isnan(info1) error('undefined length byte strings are not supported by this decoder') end
                 o = d1(1:info1);
                 d1 = d1((info1+1):end);
             elseif major == cbor.TEXT_STRING
+                if isnan(info1) error('undefined length text strings are not supported by this decoder') end
                 o = native2unicode(d1(1:info1), 'UTF-8');
                 d1 = d1((info1+1):end);
             elseif major == cbor.ARRAY
+                if isnan(info1) info1 = 1e10; end
                 o = {};
                 homogeneous = true;
                 for i=1:info1
+                    if d1(1) == 255 d1 = d1(2:end); break end
                     [o{i}, d1] = cbor.decode(d1);
                     if homogeneous && i > 1 && ~strcmp(class(o{1}),class(o{i}))
                         homogeneous = false;
@@ -113,9 +128,11 @@ classdef cbor
                     o = cell2mat(o);
                 end
             elseif major == cbor.MAP
+                if isnan(info1) info1 = 1e10; end
                 n = {};
                 v = {};
                 for i=1:info1
+                    if d1(1) == 255 d1 = d1(2:end); break end
                     [n{i}, d1] = cbor.decode(d1);
                     [v{i}, d1] = cbor.decode(d1);
                     if n{i}(1) == '_'
@@ -125,12 +142,21 @@ classdef cbor
                 end
                 o = cell2struct(v, n, 2);
             elseif major == cbor.SIMPLE_VALUE
+                if info == cbor.SINGLE_BYTE_EXTENSION
+                    % simple value 32..255 in next byte
+                    info = d1(1);
+                    d1 = d1(2:end);
+                    if info < 32 error('malformed simple value (0..31 in byte ext)') end
+                end
+
                 if info == cbor.FALSE
                     o = false;
                 elseif info == cbor.TRUE
                     o = true;
                 elseif info == cbor.NULL
                     o = NaN;
+                elseif info == cbor.UNDEFINED
+                    error('cannot decode undefined value');
                 elseif info == cbor.FLOAT16
                     f16 = as(d1(1:2), 'uint16');
                     d1 = d1(3:end);
@@ -154,12 +180,15 @@ classdef cbor
                 elseif info == cbor.FLOAT64
                     o = as(d1(1:8), 'double');
                     d1 = d1(9:end);
+                elseif info == cbor.BREAK
+                    % not used - will peek for 255 manually where needed
+                    error('unexpected break');
                 else
                     error('unrecognized simple value: %d', info);
                 end
             end
         end
-    
+
         function [d] = encode(o,varargin)
             opts = cbor.getopts(varargin ...
             );
@@ -176,11 +205,11 @@ classdef cbor
                     d = [uint8(info)];
                 elseif info < 2^8
                     d = [cbor.UINT8, uint8(info)];
-                elseif info < 2^16 
+                elseif info < 2^16
                     d = [cbor.UINT16, tobytes(uint16(info))];
-                elseif info < 2^32 
+                elseif info < 2^32
                     d = [cbor.UINT32, tobytes(uint32(info))];
-                elseif info < 2^64 
+                elseif info < 2^64
                     d = [cbor.UINT64, tobytes(uint64(info))];
                 else
                     error('info value too big: %d', info);
@@ -193,13 +222,13 @@ classdef cbor
                 );
                 return
             end
-        
+
             % encode NaN as null
             if isnumeric(o) && numel(o) == 1 && isnan(o)
                 d = [HDR(cbor.SIMPLE_VALUE, cbor.NULL)];
                 return
             end
-        
+
             % array types
             l = numel(o);
             if l == 0
@@ -226,7 +255,7 @@ classdef cbor
             elseif l > 1
                 error('cannot encode nd-matrix');
             end
-        
+
             % basic types:
             if isa(o, 'struct')
                 % map
@@ -279,7 +308,7 @@ classdef cbor
             T(false,'F4');
             T(true,'F5');
             T(NaN,'F6');
-            
+
             T(1,'01');
             T(100,'1864');
             T(1000,'1903E8');
@@ -293,9 +322,9 @@ classdef cbor
             T(2^32,'1B0000000100000000');
 
             T(3.14159,'FB400921F9F01B866E');
-            
+
             T('abc','63616263');
-            
+
             T(struct('a', 10.52),'A16161FB40250A3D70A3D70A');
             T(struct('b', true),'A16162F5');
             T(struct('c', 'foo'),'A1616363666F6F');
@@ -304,15 +333,15 @@ classdef cbor
 
             T({1,2,3},'83010203');
             T([1,2,3],'83010203');
-            
+
             T({1.23,4.56,7.89},'83FB3FF3AE147AE147AEFB40123D70A3D70A3DFB401F8F5C28F5C28F');
-            
+
             T(num2cell(2.^(0:16)),'9101020408101820184018801901001902001904001908001910001920001940001980001A00010000');
 
             T(["a","b","c"],'83616161626163');
             T({"a","b","c"},'83616161626163');
             T({'a','b','c'},'83616161626163');
-            
+
             T({0,"b",true,-1,3.98},'85006162F520FB400FD70A3D70A3D7');
 
             T(uint8([1,2,3]),'43010203');
@@ -341,6 +370,15 @@ classdef cbor
             assert(-inf==dec('F9FC00'));
             Td(0,'F98000'); % -0
             Td(-2,'F9C000');
+
+            % test for more than 24 items:
+
+            T(struct('a', 1000, 'b', 1001, 'c', 1002, 'd', 1003, 'e', 1004, 'f', 1005, 'g', 1006, 'h', 1007, 'i', 1008, 'j', 1009, 'k', 1010, 'l', 1011, 'm', 1012, 'n', 1013, 'o', 1014, 'p', 1015, 'q', 1016, 'r', 1017, 's', 1018, 't', 1019, 'u', 1020, 'v', 1021, 'w', 1022, 'x', 1023, 'y', 1024, 'z', 1025),'B81A61611903E861621903E961631903EA61641903EB61651903EC61661903ED61671903EE61681903EF61691903F0616A1903F1616B1903F2616C1903F3616D1903F4616E1903F5616F1903F661701903F761711903F861721903F961731903FA61741903FB61751903FC61761903FD61771903FE61781903FF6179190400617A190401');
+
+            % indefinite-length array and map items: (decode-only)
+
+            assert(isequalx(struct('a', 1, 'b', 2), dec('BF616101616202FF')));
+            assert(isequalx([1,2,3], dec('9F010203FF')));
 
             fprintf('All tests passed successfully.\n');
         end
