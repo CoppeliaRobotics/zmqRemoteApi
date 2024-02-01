45cd682ef6f06fc5b8c380676db676f1a8af81b0
Federico Ferri <federico.ferri.it@gmail.com>
2024-02-01 12:16:51 +0100

    move calltip module to include.git

--- tools/calltip_test.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    import sys
-    from calltip import FuncDef
-    from pprint import PrettyPrinter
-    pp = PrettyPrinter(indent=4)
-    c = 'int objectHandle=sim.getObject(string path,map options={})'
-    func_defs = FuncDef.from_calltip(c)
-    pp.pprint(func_defs)
