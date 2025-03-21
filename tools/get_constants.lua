950ab32e7b0b203f9c5c3d57c9632c5140ba840e
Coppelia <mfreese@gmx.ch>
2025-03-21 08:52:16 +0100

    fix for missing c++ constants

--- tools/get_constants.lua
+++ tools/get_constants.lua
@@ -1,20 +1,43 @@
-function stripFunctions(t)
-    if type(t)=='table' then
-        local r={}
-        for k,v in pairs(t) do
-            if type(v)~='function' then
-                r[k]=stripFunctions(v)
+function flatten(key, map)
+    if key ~= '' then
+        key = key .. '_'
+    end
+    local retMap = {}
+    for k, v in pairs(map) do
+        if type(v) == 'table' then
+            local m = flatten(key .. k, v)
+            for k2, v2 in pairs(m) do
+                retMap[k2] = v2
+            end
+        elseif type(v) == 'number' then
+            retMap[key .. k] = v
+        end
+    end
+    return retMap
+end
+
+function loadAuxModule(nm)
+    return require(nm)
+end
+
+r={}
+local allConst = sim.getApiFunc(-1, '-')
+local loadedModules = {}
+for i = 1, #allConst do
+    local v = allConst[i]
+    local dotPos = v:find('%.')
+    if v:sub(1, 3) == 'sim' then 
+        v = v:sub(1, dotPos - 1)
+        if loadedModules[v] == nil then
+            loadedModules[v] = true
+            local res, dat = pcall(loadAuxModule, v)
+            if not res then
+                print(dat)
+            else
+                r[v] = flatten('', dat)
             end
         end
-        if next(r) then return r end
-    else
-        return t
     end
 end
-r={}
-for k,v in pairs(_G) do
-    if type(v)=='table' and k:sub(1,3)=='sim' then
-        r[k]=stripFunctions(v)
-    end
-end
+
 return r
